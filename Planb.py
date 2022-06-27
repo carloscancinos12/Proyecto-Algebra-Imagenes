892a97dd8e1840dd5e0083e0762f651bf4a09854
Carlos Cancinos <61300585+carloscancinos12@users.noreply.github.com>
2022-06-27 04:17:45 -0600

    Finaaaaaaaal

--- Planb.py
+++ Planb.py
@@ -3,6 +3,7 @@ from PIL import Image, ImageTk
 import numpy as np
 import matplotlib.pyplot as plt
 import imageio.v2 as imageio
+import tkinter
 
 root = Tk()
 root.title("Imagenes Algebra")
@@ -13,12 +14,12 @@ imgIn = imageio.imread(archivo)
 recorte = imgIn[110:440, 215:525]
 
 Aument=StringVar()
-Aument_entry = Entry(root,textvariable=Aument)
+Aument_entry = tkinter.Entry(root,textvariable=Aument)
 Aument_entry.config(width=10)
 Aument_entry.place(x=220,y=80)
 
 Dismn=StringVar()
-Dismn_entry = Entry(root,textvariable=Dismn)
+Dismn_entry = tkinter.Entry(root,textvariable=Dismn)
 Dismn_entry.config(width=10)
 Dismn_entry.place(x=30,y=80)
 
@@ -61,22 +62,75 @@ def rotarT(Matriz):
     rotada = reflejarY(rotada)
     return rotada
 
+def aumentarFilas(Matriz, aumentoD):
+    f, c, p = Matriz.shape
+    duplicada = np.zeros(((aumentoD * f), c, p), int)
+    for capa in range(0,p):
+        for fila in range(0,f):
+            for aum in range(0,aumentoD):
+                duplicada[((fila*aumentoD) + aum),:,capa] = Matriz[fila,:,capa]
+    return duplicada
+
+def aumentarColumnas(Matriz, aumentoD):
+    f, c, p = Matriz.shape
+    duplicada = np.zeros((f, (c * aumentoD), p), int)
+    for capa in range(0,p):
+        for columna in range(0,c):
+            for aum in range(0,aumentoD):
+                duplicada[:,((columna*aumentoD) + aum),capa] = Matriz[:,columna,capa]
+    return duplicada
+
+def aumentarDimensiones(Matriz, aumentoD):
+    f, c, p = Matriz.shape
+    aumentada = np.zeros(((f + (f//aumentoD)), (c + (c//aumentoD)), p), int)
+    aumentada = aumentarFilas(Matriz, aumentoD)
+    aumentada = aumentarColumnas(aumentada, aumentoD)
+    return aumentada
+
+def disminuirFilas(Matriz, disminucionD):
+    f, c, p = Matriz.shape
+    duplicada = np.zeros((int(f / disminucionD), c, p), int)
+    for capa in range(0,p):
+        for fila in range(0,f):
+            if (fila%disminucionD) == 0:
+                duplicada[int(fila / disminucionD),:,capa] = Matriz[fila,:,capa]
+    return duplicada
+
+def disminuirColumnas(Matriz, disminucionD):
+    f, c, p = Matriz.shape
+    duplicada = np.zeros((f, int(c / disminucionD), p), int)
+    for capa in range(0,p):
+        for columna in range(0,c):
+            if (columna%disminucionD) == 0:
+                duplicada[:,int(columna / disminucionD),capa] = Matriz[:,columna,capa]
+    return duplicada
+
+def disminuirDimensiones(Matriz, aumentoD):
+    f, c, p = Matriz.shape
+    aumentada = np.zeros(((f + (f//aumentoD)), (c + (c//aumentoD)), p), int)
+    aumentada = disminuirFilas(Matriz, aumentoD)
+    aumentada = disminuirColumnas(aumentada, aumentoD)
+    return aumentada
 
 def ComandoHorario():
     plt.figure(figsize=(4,4))
-    plt.imshow(rotarH(recorte))
+    plt.imshow(rotarH(imgIn))
     plt.show()
     
 def ComandoAnHorario():
     plt.figure(figsize=(4,4))
-    plt.imshow(rotarAH(recorte))
+    plt.imshow(rotarAH(imgIn))
     plt.show()
     
 def ComandoAumentar():
-    None
+    plt.figure(figsize=(4,4))
+    plt.imshow(aumentarDimensiones(imgIn,int(Aument_entry.get())))
+    plt.show()
     
 def ComandoReducir():
-    None
+    plt.figure(figsize=(4,4))
+    plt.imshow(disminuirDimensiones(imgIn,int(Dismn_entry.get())))
+    plt.show()
     
 def ComandoOrig():
     plt.figure(figsize=(4,4))
@@ -92,11 +146,11 @@ boton2 = Button(root, text="Rotar Horario", command=ComandoHorario)
 boton2.pack()
 boton2.place(x=220,y=20)
 
-boton3 = Button(root, text="Disminuir tamaño", command=ComandoAumentar)
+boton3 = Button(root, text="Disminuir tamaño", command=ComandoReducir)
 boton3.pack()
 boton3.place(x=30,y=100)
 
-boton4 = Button(root, text="Aumentar tamaño", command=ComandoReducir)
+boton4 = Button(root, text="Aumentar tamaño", command=ComandoAumentar)
 boton4.pack()
 boton4.place(x=220,y=100)
 
